bc6c140f78dd1f23e47901ffc57fb1a691824572
Unknown <spaminol@gmail.com>
2018-02-20 11:24:29 -0800

    Converted all functions to methods

--- php/db.php
+++ php/db.php
@@ -1,8 +1,7 @@
 <?php
-/*
+require_once('config.php');
 class db
 {
-	require_once('config.php');
 	public $db_handler;
 	private $config;
 
@@ -19,115 +18,40 @@ class db
 		};
 	}
 
-	function __get($query)
-	{
-		$items = $db_handler->prepare($query);
+	function getTasks() {
+		$items = $this->db_handler->prepare("SELECT * FROM `tasks`");
 		$items->execute();
 		$items = $items->fetchAll(PDO::FETCH_OBJ);
+
 		return $items;
 	}
 
-	function __set($query)
-	{
-		
+	function addTask($name, $description) {
+		$items = $this->db_handler->prepare("INSERT INTO `tasks` (`name`, `description`) VALUES ('$name', '$description')");
+		$items->execute();
+
+		return header('Location: http://127.0.0.1/todo');
 	}
+
+	function deleteTask($id) {
+		$items = $this->db_handler->prepare("DELETE FROM `tasks` WHERE `tasks`.`id` = $id");
+		$items->execute();
+
+		return header('Location: http://127.0.0.1/todo');
+	}
+
+	function completeTask($id) {
+		$items = $this->db_handler->prepare("UPDATE `tasks` SET `status`= 'complete' WHERE `id` = $id");
+		$items->execute();
+
+		return header('Location: http://127.0.0.1/todo');
+	}
+
+	function incompleteTask($id) {
+		$items = $this->db_handler->prepare("UPDATE `tasks` SET `status`= 'incomplete' WHERE `id` = $id");
+		$items->execute();
+
+		return header('Location: http://127.0.0.1/todo');
+	}
+
 }
-*/
-require_once ('config.php');
-
-function getTasks() {
-	try {
-		$config = new dbConfig;
-		$db_connection = new PDO("mysql:host=$config->host;dbname=$config->db", $config->user, $config->pass);
-		$db_connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-		$items = $db_connection->prepare("SELECT * FROM `tasks`");
-		$items->execute();
-		$items = $items->fetchAll(PDO::FETCH_OBJ);
-
-		return $items;
-	}
-	catch (Exception $e) {
-		print "Error: " . $e->getMessage();
-		die();
-	};
-
-};
-
-function addTask($name, $description) {
-	try {
-		$config = new dbConfig;
-		$db_connection = new PDO("mysql:host=$config->host;dbname=$config->db", $config->user, $config->pass);
-		$db_connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-		$query = "INSERT INTO `tasks` (`name`, `description`) VALUES ('$name', '$description')";
-
-		$items = $db_connection->exec($query);
-
-		return header('Location: http://127.0.0.1/todo');
-	}
-	catch (Exception $e) {
-		print "Error: " . $e->getMessage();
-		die();
-	};
-
-};
-
-function deleteTask($id) {
-	try {
-		$config = new dbConfig;
-		$db_connection = new PDO("mysql:host=$config->host;dbname=$config->db", $config->user, $config->pass);
-		$db_connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-		$query = "DELETE FROM `tasks` WHERE `tasks`.`id` = $id";
-
-		$items = $db_connection->exec($query);
-
-		return header('Location: http://127.0.0.1/todo');
-	}
-	catch (Exception $e) {
-		print "Error: " . $e->getMessage();
-		die();
-	};
-
-};
-
-function completeTask($id) {
-	try {
-		$config = new dbConfig;
-		$db_connection = new PDO("mysql:host=$config->host;dbname=$config->db", $config->user, $config->pass);
-		$db_connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-		$query = "UPDATE `tasks` SET `status`= 'complete' WHERE `id` = $id";
-
-		$items = $db_connection->exec($query);
-
-		return header('Location: http://127.0.0.1/todo');
-	}
-	catch (Exception $e) {
-		print "Error: " . $e->getMessage();
-		die();
-	};
-
-};
-
-function incompleteTask($id) {
-	try {
-		$config = new dbConfig;
-		$db_connection = new PDO("mysql:host=$config->host;dbname=$config->db", $config->user, $config->pass);
-		$db_connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-		$query = "UPDATE `tasks` SET `status`= 'incomplete' WHERE `id` = $id";
-
-		$items = $db_connection->exec($query);
-
-		return header('Location: http://127.0.0.1/todo');
-	}
-	catch (Exception $e) {
-		print "Error: " . $e->getMessage();
-		die();
-	};
-
-};
-
-?>
